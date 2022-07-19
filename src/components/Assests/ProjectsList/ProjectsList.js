4355bb88a25ad51efee855533f6a20d644e18c0c
Rko_8001 <yadwindersingh0705@gmail.com>
2022-07-19 11:28:29 +0530

    added the projects

---
+++ src/components/Assests/ProjectsList/ProjectsList.js
@@ -0,0 +1,35 @@
+
+export const projectList= [
+    {
+      title: "Basic C Compiler",
+      subtitle: "Lexx and Yacc",
+      description:
+        "implemented the lexical as well as syntax analyser for basic c language. During Lexical analysis, input code is tokenized the lexems acc. to tokens. During Syntax analysis, code is parsed and grammer of code is checked.",
+      link: "https://github.com/Rko-8001/C-Compiler",
+    },
+    {
+      title: "Online Code Editor",
+      subtitle: "React",
+      description:
+        "This Editor Lets You Code And View The Changes In Your HTML,CSS and javascript code all at the same time. It's perfect for any developer who wants to write and run some code without opening a Applicaton.",
+  
+      link: "https://github.com/Rko-8001/Online-Code-Editor-",
+    },
+    {
+      title: "Snippet Generator",
+      subtitle: "React",
+      description:
+        "Snippet Generator lets you create you snippets for a piece of code you often use. This helps to save time and also enchance your programming experience.",
+  
+      link: "https://github.com/Rko-8001/Snippet-Generator",
+    },
+    {
+      title: "Tetris Game",
+      subtitle: "HTML, CSS, Javascript",
+      description:
+        "Traditional Tetris Game made using javascript. There are 6 types of tetrimino and you can rotate them around axis. Game ends if the tetrimino fills any one of the row.",
+  
+      link: "https://github.com/Rko-8001/Tetris-Game",
+    },
+  ];
+  
--- src/components/MyProjects/MyProjects.js
+++ src/components/MyProjects/MyProjects.js
@@ -1,9 +1,41 @@
 import React from 'react';
-
+import { projectList } from "../Assests/ProjectsList/ProjectsList";
+import { HiCode } from "react-icons/hi";
 function MyProjects() {
     return (
+
         <>
-        MyProjects
+            <section id="projects" className="text-[#ff0000] bg-black body-font">
+                <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+                    <div className="flex flex-col w-full mb-20">
+                        <HiCode className="mx-auto inline-block h-20 w-10 mb-4" />
+                        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                            Projects I've Contributed To
+                        </h1>
+
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        {projectList.map((project) => (
+                            <a
+                                href={project.link}
+                                key={project.subtitle}
+                                className="sm:w-1/2 w-100 p-2 ">
+                                <div className="flex relative">
+                                    <div className="px-8 py-10 relative z-10 w-full rounded bg-zinc-700 ">
+                                        <h1 className="title-font text-lg font-medium text-[#ff0000]mb">
+                                            {project.title}
+                                        </h1>
+                                        <h2 className="tracking-widest text-sm title-font font-medium text-white mb-4">
+                                            {project.subtitle}
+                                        </h2>
+                                        <p className="leading-relaxed text-white">{project.description}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
